c351113e257cbd9e3e92f74834355ebcb699f12d
Lucas Fischer <fischer-lucas@hotmail.com>
2022-04-23 20:12:13 +0100

    update estudos

---
+++ Placeholders/Placeholder.py
@@ -0,0 +1,13 @@
+valor = False
+
+# PlaceHolder utilizando o comando pass.
+if valor:
+    pass
+else:
+    print('sem retorno')
+
+# Placeholder utilizando o comando ellipsis
+if valor:
+    ...
+else:
+    print('sem retorno')
