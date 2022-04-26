d5ec207f6fc91c6faa394d3d1b4d06d8506fc4ab
Lucas Fischer <fischer-lucas@hotmail.com>
2022-04-27 00:30:40 +0100

    update

---
+++ Strings/Manipulacao_Strings.py
@@ -0,0 +1,11 @@
+# Índice positivo da String.
+texto = "O rato roeu a roupa do rei de Roma."
+print(texto[3])
+
+# Índice negativo da String.
+site = 'www.google.com.br/'
+print(site[:-1])
+
+# Fatiamento da string.
+nova_string = texto[2:7]
+print(nova_string)
